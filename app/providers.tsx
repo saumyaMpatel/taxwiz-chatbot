5f5aea9aedcb475ab485fda7f7aab10782b0d416
HreemPandya <hreempandya@gmail.com>
2025-08-11 22:28:41 -0400

    Attribute Fix

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,21 +1,12 @@
 "use client"
 
 import type React from "react"
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ThemeProvider } from "@/components/theme-provider"
-import { useState } from "react"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  // Create a client
-  const [queryClient] = useState(() => new QueryClient())
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-        {children}
-      </ThemeProvider>
-    </QueryClientProvider>
+    <ThemeProvider defaultTheme="light" enableSystem>
+      {children}
+    </ThemeProvider>
   )
 }
-
